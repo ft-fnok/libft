cd8303eb5803f03dcd97a8f414ba80ec802a09e2
nlalleik <nlalleik@students.42wolfsburg.de>
2021-12-19 22:46:37 +0100

    minor adaption

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: nlalleik <nlalleik@students.42wolfsburg.de +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 18:10:09 by nlalleik          #+#    #+#             */
-/*   Updated: 2021/12/05 20:26:23 by nlalleik         ###   ########.fr       */
+/*   Updated: 2021/12/19 22:43:16 by nlalleik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char	*string;
 	size_t	len;
 
-	if (!s)
+	if (!s || !f)
 		return (NULL);
 	len = ft_strlen(s);
 	string = ft_calloc(len + 1, sizeof(char));
